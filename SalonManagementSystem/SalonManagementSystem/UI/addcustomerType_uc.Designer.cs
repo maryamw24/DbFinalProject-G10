59aba6387da1feba503297a76a6ba0853267dfad
Maryam Waseem <maryamw1124@gmail.com>
2024-04-29 15:01:11 +0500

    add customer Type UI implemented.

---
+++ SalonManagementSystem/SalonManagementSystem/UI/addcustomerType_uc.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace SalonManagementSystem.UI
+{
+    partial class addcustomerType_uc
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.fulllNameTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.emialTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.phoneTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.guna2ShadowPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(233)))), ((int)(((byte)(236)))));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 683F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 699F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 170F));
+            this.tableLayoutPanel1.Controls.Add(this.guna2ShadowPanel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 510F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 175F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1588, 676);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // guna2ShadowPanel1
+            // 
+            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.IndianRed;
+            this.guna2ShadowPanel1.Controls.Add(this.tableLayoutPanel2);
+            this.guna2ShadowPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(155)))));
+            this.guna2ShadowPanel1.Location = new System.Drawing.Point(686, 69);
+            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            this.guna2ShadowPanel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel1.ShadowDepth = 20;
+            this.guna2ShadowPanel1.ShadowStyle = Guna.UI2.WinForms.Guna2ShadowPanel.ShadowMode.ForwardDiagonal;
+            this.guna2ShadowPanel1.Size = new System.Drawing.Size(693, 504);
+            this.guna2ShadowPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 104F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 485F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 104F));
+            this.tableLayoutPanel2.Controls.Add(this.fulllNameTxt, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.emialTxt, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.phoneTxt, 1, 6);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.addBtn, 1, 8);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 11;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 74F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 19F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 19F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(693, 504);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // fulllNameTxt
+            // 
+            this.fulllNameTxt.BorderThickness = 0;
+            this.fulllNameTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fulllNameTxt.DefaultText = "";
+            this.fulllNameTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.fulllNameTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.fulllNameTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.fulllNameTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.fulllNameTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fulllNameTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.fulllNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fulllNameTxt.ForeColor = System.Drawing.Color.Black;
+            this.fulllNameTxt.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.fulllNameTxt.Location = new System.Drawing.Point(107, 126);
+            this.fulllNameTxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.fulllNameTxt.Name = "fulllNameTxt";
+            this.fulllNameTxt.PasswordChar = '\0';
+            this.fulllNameTxt.PlaceholderForeColor = System.Drawing.Color.Gray;
+            this.fulllNameTxt.PlaceholderText = "Name *";
+            this.fulllNameTxt.SelectedText = "";
+            this.fulllNameTxt.ShadowDecoration.Depth = 50;
+            this.fulllNameTxt.Size = new System.Drawing.Size(479, 44);
+            this.fulllNameTxt.TabIndex = 0;
+            // 
+            // emialTxt
+            // 
+            this.emialTxt.BorderThickness = 0;
+            this.emialTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.emialTxt.DefaultText = "";
+            this.emialTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.emialTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.emialTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.emialTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.emialTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.emialTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.emialTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emialTxt.ForeColor = System.Drawing.Color.Black;
+            this.emialTxt.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.emialTxt.Location = new System.Drawing.Point(107, 197);
+            this.emialTxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.emialTxt.Name = "emialTxt";
+            this.emialTxt.PasswordChar = '\0';
+            this.emialTxt.PlaceholderForeColor = System.Drawing.Color.Gray;
+            this.emialTxt.PlaceholderText = "Discount Percentage *";
+            this.emialTxt.SelectedText = "";
+            this.emialTxt.ShadowDecoration.Depth = 50;
+            this.emialTxt.Size = new System.Drawing.Size(479, 43);
+            this.emialTxt.TabIndex = 1;
+            // 
+            // phoneTxt
+            // 
+            this.phoneTxt.BorderThickness = 0;
+            this.phoneTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.phoneTxt.DefaultText = "";
+            this.phoneTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.phoneTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.phoneTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.phoneTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.phoneTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.phoneTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.phoneTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneTxt.ForeColor = System.Drawing.Color.Black;
+            this.phoneTxt.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.phoneTxt.Location = new System.Drawing.Point(107, 267);
+            this.phoneTxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.phoneTxt.Name = "phoneTxt";
+            this.phoneTxt.PasswordChar = '\0';
+            this.phoneTxt.PlaceholderForeColor = System.Drawing.Color.Gray;
+            this.phoneTxt.PlaceholderText = "Minimum appointments to reach this type *";
+            this.phoneTxt.SelectedText = "";
+            this.phoneTxt.ShadowDecoration.Depth = 50;
+            this.phoneTxt.Size = new System.Drawing.Size(479, 44);
+            this.phoneTxt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 74);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Add Customer Type Details";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // addBtn
+            // 
+            this.addBtn.BorderRadius = 25;
+            this.addBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.addBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.addBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.addBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.addBtn.FillColor = System.Drawing.Color.IndianRed;
+            this.addBtn.Font = new System.Drawing.Font("Tw Cen MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.ForeColor = System.Drawing.Color.Black;
+            this.addBtn.Location = new System.Drawing.Point(107, 360);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(474, 65);
+            this.addBtn.TabIndex = 7;
+            this.addBtn.Text = "Add ";
+            // 
+            // addcustomerType_uc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "addcustomerType_uc";
+            this.Size = new System.Drawing.Size(1588, 676);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.guna2ShadowPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private Guna.UI2.WinForms.Guna2TextBox fulllNameTxt;
+        private Guna.UI2.WinForms.Guna2TextBox emialTxt;
+        private Guna.UI2.WinForms.Guna2TextBox phoneTxt;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button addBtn;
+    }
+}
